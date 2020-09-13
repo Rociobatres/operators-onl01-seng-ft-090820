8cf7666d05d140d9dc80b1a14846d01f2d937061
Rocio Batres <jonasgirl21@gmail.com>
2020-09-13 02:15:00 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,19 @@
+
 def unsafe?(speed)
-
-end
-
+  if speed < 40 
+    return true 
+  elsif speed > 60 
+    return true
+  else
+    return false
+  end
 
 
 def not_safe?(speed)
-	
+  return speed < 40 || speed > 60 ? true : false
+  end
 end
-	
+
+
 
 
